30ccf9a2ece26cc496ae4c87522fa8aa7b55cd46
Diego Chiodi Gonçalves <dchiodigoncalves@gmail.com>
2025-05-14 16:23:11 -0300

    Render do GameObject implementada

--- Golf/src/GameObject.cpp
+++ Golf/src/GameObject.cpp
@@ -4,4 +4,8 @@ GameObject::GameObject(float x, float y, CollisionType collisionType)
     : x(x), y(y), collisionType(collisionType)
 {}
 
+int GameObject::Render(SDL_Renderer* renderer) {
+    // Implementar lógica de renderização do objeto
+    return 0;
+}
 GameObject::~GameObject() {}
--- Golf/src/circle.cpp
+++ Golf/src/circle.cpp
@@ -7,7 +7,6 @@ Circle::Circle(float x, float y, CollisionType collisionType, SDL_Color color, f
 int Circle::Render(SDL_Renderer* renderer) {
     // Implementar lógica de renderização da bola
     filledCircleColor(renderer, x, y, 10, 0x0000FFFF);
-
     return 0;
 }
 
