c316d0ac1c25025473ea6a7cdcfa8ab8f9298a5b
Diego Chiodi Gonçalves <dchiodigoncalves@gmail.com>
2025-05-21 16:03:37 -0300

    compile

--- src/Player.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Movel.h"
-
-class Player : public Movel {
-    public:
-    Player(float x, float y, float width, float height, SDL_Color color, vector speed);
-    void HandleEvents(SDL_Event& event) override;
-};
